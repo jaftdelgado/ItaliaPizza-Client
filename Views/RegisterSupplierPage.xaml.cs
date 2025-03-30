28376f7256d7c5ec364e878ae526e06c63565385
x3776x <abrahamvvquinto@gmail.com>
2025-03-29 18:26:03 -0600

    Ventanas para CU-07 y CU-22.

---
+++ Views/RegisterSupplierPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizzaClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para RegisterSupplierPage.xaml
+    /// </summary>
+    public partial class RegisterSupplierPage : Page
+    {
+        public RegisterSupplierPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnableSupplyBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveSupplierBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegSupplyBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelRegister(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
