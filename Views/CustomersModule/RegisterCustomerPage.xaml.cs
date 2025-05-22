e23d6fad1c3d1fb690bdb9900d2d54f7a4883212
x3776x <abrahamvvquinto@gmail.com>
2025-05-21 19:43:12 -0600

    add Gestion clientes

--- Views/CustomersModule/RegisterCustomerPage.xaml.cs
+++ Views/CustomersModule/RegisterCustomerPage.xaml.cs
@@ -2,6 +2,7 @@
 using ItaliaPizzaClient.Model;
 using ItaliaPizzaClient.Utilities;
 using ItaliaPizzaClient.Views.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +25,46 @@ namespace ItaliaPizzaClient.Views
             UpdateRegisterButtonState();
         }
 
+        public RegisterCustomerPage(Customer editingCustomer)
+        {
+            InitializeComponent();
+            _isEditMode = true;
+            _editingCustomer = editingCustomer;
+
+            ConfigureInterfaceForMode();
+            SetInputFields();
+            LoadCustomerData(editingCustomer);
+            UpdateFormButtonState(_isEditMode ? BtnEditCustomer : BtnRegisterCostumer);
+        }
+
+        private void ConfigureInterfaceForMode()
+        {
+            if (_isEditMode)
+            {
+                PageHeader.SetResourceReference(TextBlock.TextProperty, "EditCustomer_Header");
+                PageDescription.SetResourceReference(TextBlock.TextProperty, "EditCustomer_Desc");
+                BtnEditCustomer.Visibility = Visibility.Visible;
+                BtnRegisterCostumer.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                PageHeader.SetResourceReference(TextBlock.TextProperty, "RegCustomer_Header");
+                PageDescription.SetResourceReference(TextBlock.TextProperty, "RegCustomer_Desc");
+                BtnEditCustomer.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void LoadCustomerData(Customer customer)
+        {
+            TbCostumerName.Text = customer.FirstName;
+            TbLastName.Text = customer.LastName;
+            TbPhoneNumber.Text = customer.PhoneNumber;
+            TbEmailAddress.Text = customer.EmailAddress;
+            TbAddress.Text = customer.Address.AddressName;
+            TbZipCode.Text = customer.Address.ZipCode;
+            TbCity.Text = customer.Address.City;
+        }
+
         private void SetInputFields()
         {
             InputUtilities.ValidateInput(TbCostumerName, Constants.NAMES_PATTERN, Constants.MAX_LENGTH_NAMES);
@@ -35,6 +76,29 @@ namespace ItaliaPizzaClient.Views
             InputUtilities.ValidateInput(TbCity, Constants.GENERAL_TEXT_PATTERN, Constants.MAX_LENGTH_CITY);
 
             InputUtilities.ConvertToLowerCase(TbEmailAddress);
+        }
+
+        private void UpdateFormButtonState(Button button)
+        {
+            var requiredFields = new List<TextBox>
+            {
+                TbCostumerName, TbLastName,
+                TbPhoneNumber, TbEmailAddress,
+                TbAddress, TbZipCode, TbCity
+            };
+
+            bool allFieldsFilled = true;
+
+            foreach (var field in requiredFields)
+            {
+                if (string.IsNullOrWhiteSpace(field.Text))
+                {
+                    allFieldsFilled = false;
+                    break;
+                }
+            }
+
+            button.IsEnabled = allFieldsFilled;
         }
 
         private void UpdateRegisterButtonState()
@@ -128,5 +192,61 @@ namespace ItaliaPizzaClient.Views
             UpdateRegisterButtonState();
         }
         #endregion
+
+        private async Task EditCustomer()
+        {
+            bool success = false;
+            string validationErrorKey = null;
+            string validationDescriptionKey = null;
+
+            var updateDTO = new CustomerDTO
+            {
+                CustomerID = _editingCustomer.CustomerID,
+                FirstName = _editingCustomer.FirstName,
+                LastName = _editingCustomer.LastName,
+                PhoneNumber = _editingCustomer.PhoneNumber,
+                EmailAddress = _editingCustomer.EmailAddress,
+                Address = new AddressDTO
+                {
+                    Id = _editingCustomer.AddressID,
+                    AddressName = TbAddress.Text.Trim(),
+                    ZipCode = TbZipCode.Text.Trim(),
+                    City = TbCity.Text.Trim()
+                }
+            };
+
+            await ServiceClientManager.ExecuteServerAction(async () =>
+            {
+                var client = ServiceClientManager.Instance.Client;
+                if (client == null) return;
+
+                if (!string.Equals(_editingCustomer.EmailAddress, updateDTO.EmailAddress,
+                    StringComparison.OrdinalIgnoreCase) && !client.IsCustomerEmailAvailable(updateDTO.EmailAddress))
+                {
+                    validationErrorKey = "GlbDialogD_EmailDuplicate";
+                    validationDescriptionKey = "GlbDialogD_EmailDuplicate";
+                    return;
+                }
+
+                success = await client.UpdateCustomerAsync(updateDTO);
+            });
+
+            if (!string.IsNullOrEmpty(validationErrorKey))
+            {
+                MessageDialog.Show(validationErrorKey, validationDescriptionKey, AlertType.WARNING);
+                return;
+            }
+
+            if (success)
+            {
+                MessageDialog.Show("RegCustomer_DialogTEditSuccess", "RegCustomer_DialogDEditSuccess",
+                    AlertType.SUCCESS, () => NavigationManager.Instance.GoBack());
+            }
+        }
+
+        private async void Click_BtnEditCustomer(object sender, RoutedEventArgs e)
+        {
+            await EditCustomer();
+        }
     }
 }
